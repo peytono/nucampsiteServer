80923f80c756faececd7f6419063e99a5f93529c
Peyton <pcopcoeco@gmail.com>
2020-10-02 19:27:49 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1820617231422969',
+        clientSecret: '3522d9f2c9d16fe97d5f82699554ee92'
+    }
 }
